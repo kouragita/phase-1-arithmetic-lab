1e00cf17c7be1917bb84d882b5079650ea8cc5c8
wilson mwangi waweru <kouragitam@gmail.com>
2024-06-25 02:42:58 +0300

    aritmetic operations

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+// multiply
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2; // 31 * 2 = 62
+
+// random
+const random = Math.floor(Math.random() * 100) + 1; // Generates a random integer greater than 0
+
+//mod
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4; // 10 % 6 = 4
+
+//max
+const max = Math.max(1, 2, 3, 4, 20, 5); // 20 is the highest number in the set
